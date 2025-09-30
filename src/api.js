e79fac0210808e9b46397968007df525c52c27f4
yui <kimmy@knu.ua>
2025-09-30 16:10:32 +0300

    City search with autocomplete and api keys define

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '92f7725fb1mshc0820b68a35dabcp11e5a5jsnb4ef9f4eb317',
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d5b05c13ffc0747eaa771d84b8e923d3";
